db497a3588d36a2b6bc0a7f943b711defc563133
Fotis Evangelou <fevangelou@users.noreply.github.com>
2016-11-18 15:43:44 +0200

    Remove unneeded files

--- 2016/index_files/custom.js
+++
@@ -1,9 +0,0 @@
-/**
- * @version		1.0.0
- * @package		Ibento
- * @author		Nuevvo - http://nuevvo.com
- * @copyright Copyright (c) 2010 - 2014 Nuevvo Webware Ltd. All rights reserved.
- * @license		http://nuevvo.com/license
- */
-
-/* Add custom JS code here for easier template updates */
